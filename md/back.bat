fea5664510f21c9616fd205fd72648fdbce346c5
yjh <yjhixy@163.com>
2023-08-10 22:06:08 +0800

    init

---
+++ md/back.bat
@@ -0,0 +1,13 @@
+@echo off
+
+move edit.html bk/edit.html
+move new.html bk/new.html
+move view.html bk/view.html
+move viewex.html bk/viewex.html
+
+
+rename edit.min.html edit.html
+rename new.min.html new.html
+rename view.min.html view.html
+rename viewex.min.html viewex.html
+pause
